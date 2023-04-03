bef685170e664b8063ade7316f65cc73c913b805
Alexander Mee <mralexandermee@gmail.com>
2023-04-04 00:42:26 +0100

    this works, except for an ssl issue with /login/

--- dhb/__init__.py
+++ dhb/__init__.py
@@ -5,12 +5,14 @@ from flask import Flask
 from .ext import db
 from .main.auth_goog import auth_goog
 from .main.index import index
+from .main.test import test
 
 #create_app is a custom function, by convention called create_app, it initalises flask, extentions, config, blueprints
 def create_app():
     app = Flask(__name__)
 
-    app.register_blueprint(index)    
+    app.register_blueprint(index)
+    app.register_blueprint(test)       
     app.register_blueprint(auth_goog)
     
     #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///db.sqllite3'
